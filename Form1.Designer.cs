1d42c1403ab829e077e2416dac2c9caf4b4508ff
domin <domin@DESKTOP-18O9S4I>
2024-06-05 21:22:29 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "generuj",
+            "oblicz"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 50);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(121, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(474, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Marti";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // Ustawienie zakresu dla TrackBar
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 5;
+            trackBar1.Value = 1;
+
+            // Inicjalizacja DataGridView
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.RowCount = 5;
+
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            trackBar1.ValueChanged += TrackBar1_ValueChanged;
+            textBox2.TextChanged += TextBox2_TextChanged;
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "generuj")
+            {
+                GenerateRandomNumbers();
+            }
+            else if (comboBox1.SelectedItem.ToString() == "oblicz")
+            {
+                CalculateResult();
+            }
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+            label1.Text = "";
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void GenerateRandomNumbers()
+        {
+            if (double.TryParse(textBox1.Text, out double maxRange))
+            {
+                double[] values = new double[25];
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = random.NextDouble() * (maxRange + 2) - 2; // Random double between -2 and maxRange
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        dataGridView1[j, i].Value = values[i * 5 + j];
+                    }
+                }
+
+                double average = values.Average();
+                MessageBox.Show($"Średnia: {average}");
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź prawidłową liczbę do TextBox1");
+            }
+        }
+
+        private void CalculateResult()
+        {
+            if (double.TryParse(textBox2.Text, out double inputValue))
+            {
+                double trackBarValue = trackBar1.Value;
+                double result;
+
+                if (trackBarValue % 2 == 0)
+                {
+                    result = inputValue - trackBarValue;
+                    label1.Text = $"{inputValue} - {trackBarValue} = {result}";
+                }
+                else
+                {
+                    result = inputValue + trackBarValue;
+                    label1.Text = $"{inputValue} + {trackBarValue} = {result}";
+                }
+
+                SaveToFile(trackBarValue, inputValue, result);
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź prawidłową liczbę do TextBox2");
+            }
+        }
+
+        private void SaveToFile(double trackBarValue, double inputValue, double result)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files|*.txt";
+                saveFileDialog.Title = "Zapisz wynik";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string operation = trackBarValue % 2 == 0 ? "-" : "+";
+                    string content = $"{inputValue} {operation} {trackBarValue} = {result}";
+                    System.IO.File.WriteAllText(saveFileDialog.FileName, content);
+                }
+            }
+        }
+    }
+}
